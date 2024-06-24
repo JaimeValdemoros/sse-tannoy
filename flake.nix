0fad82ad1e08559a5e92b90453d94d9e10fa0634
Jaime Valdemoros <JaimeValdemoros@users.noreply.github.com>
2024-06-24 09:00:22 +0100

    Hook in treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,30 +1,48 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default-linux";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     naersk.url = "github:nix-community/naersk";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, naersk }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
+  outputs = inputs @ {
+    self,
+    systems,
+    nixpkgs,
+    ...
+  }: let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
 
-        naersk' = pkgs.callPackage naersk {};
+    naersk = eachSystem (pkgs: pkgs.callPackage inputs.naersk {});
 
-      in rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-        };
+    treefmt-config = {pkgs, ...}: {
+      projectRootFile = "flake.nix";
+      programs.alejandra.enable = true;
+      programs.rustfmt.enable = true;
+    };
+    treefmtEval = eachSystem (pkgs: inputs.treefmt-nix.lib.evalModule pkgs treefmt-config);
+  in {
+    # For `nix build` & `nix run`:
+    defaultPackage = eachSystem (pkgs:
+      naersk.${pkgs.system}.buildPackage {
+        src = ./.;
+      });
 
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
-        };
-      }
-    );
+    # For `nix develop`:
+    devShell = eachSystem (pkgs:
+      pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [rustc cargo];
+      });
+
+    # for `nix fmt`
+    formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+
+    # for `nix flake check`
+    checks = eachSystem (pkgs: {
+      formatting = treefmtEval.${pkgs.system}.config.build.check self;
+    });
+  };
 }
